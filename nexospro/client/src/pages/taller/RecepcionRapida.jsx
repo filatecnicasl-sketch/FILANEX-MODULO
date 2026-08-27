6dac34c720f6cea361d798b2bc4a55b19034efbf
filatecnicasl-sketch <filatecnicasl@gmail.com>
2026-08-27 10:10:59 +0200

    fix: matricula no fuerza doble columna en movil (recepcion rapida)

--- nexospro/client/src/pages/taller/RecepcionRapida.jsx
+++ nexospro/client/src/pages/taller/RecepcionRapida.jsx
@@ -261,7 +261,7 @@ export default function RecepcionRapida({ onCerrar, onCreada }) {
           )}
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
-            <div className="col-span-2">
+            <div className="sm:col-span-2">
               <label className="text-sm text-slate-400 block mb-1">Matrícula *</label>
               <BuscadorEntidad
                 opciones={vehiculos
